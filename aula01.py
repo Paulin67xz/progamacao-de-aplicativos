595582dbd07ac111be049476dae1abf909150788
Paulo Arthur <pauloarthurlimagomes2009@gmail.com>
2026-02-19 10:04:55 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteira
+peso = 61.9 # variável float
+nome = "Paulo Arthur de Lima Gomes" # variável string
+sexo_masculino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_masculino)
