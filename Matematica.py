8e3cd0a25c37f26d55679514e41f169ebafe4e01
unknown <pauloarthurlimagomes2009>
2026-02-20 09:50:48 -0300

    aula matematica

---
+++ Matematica.py
@@ -0,0 +1,13 @@
+valor1 = int(input("digite o valor1"))
+valor2 = int(input("digite o valor2"))
+
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+divisao = valor1/valor2
+multiplicacao = valor1*valor2
+
+print ("A sua soma é", soma)
+print ("A sua subtração é", subtracao)
+print("A sua divisão é", divisao)
+print("A sua multiplicação", multiplicacao)
---
+++ aluno.py
@@ -0,0 +1,9 @@
+Nome = input("digite seu nome")
+Telefone = int(input("digite seu telefone"))
+Idade = int(input("digite sua idade"))
+Cidade = input("digite sua cidade")
+
+print ("Seu nome é", Nome)
+print("Seu telefone é",Telefone)
+print("Sua idade é",Idade)
+print("Sua cidade é",Cidade)
