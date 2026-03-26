3d34f5c74ad7e0436ebe127c6aeef0a8479bf038
Paulo Arthur <pauloarthurlimagomes2009@gmail.com>
2026-03-26 10:07:44 -0300

    26/03

---
+++ estoque.py
@@ -0,0 +1,5 @@
+listas = [10, 20, 30, 40, 50]
+soma = 0
+for lista in listas:
+    soma+=lista
+print(soma)
---
+++ sistemalogin.py
@@ -0,0 +1,6 @@
+entrada = ""
+while entrada != "neymar":
+    entrada = input("Qual é a senha ")
+    print(f"Você digitou:", entrada)
+
+print("Acesso liberado")
