22d8221397fb5e8d6fdf5bf546020428ecf3a0d5
paulo <pauloarthurlimagomes2009@gmail.com>
2026-07-02 09:52:19 -0300

    0207

--- caçaBugs.py/11.py
+++ caçaBugs.py/11.py
@@ -4,8 +4,11 @@ def listar_alunos_e_turmas():
     conexao = sqlite3.connect('sistema_escola.db')
     cursor = conexao.cursor()
 
-    cursor.execute("SELECT alunos.nome, turmas.nome_turma FROM alunos INNER JOIN turmas")
+    cursor.execute("SELECT alunos.nome, turmas.nome_turma FROM alunos INNER JOIN turmas ON alunos.id_turma = turmas.id")
 
     for linha in cursor.fechall():
         print(f"Aluno: {linha[0]} | Turma:{linha[1]}")
     conexao.close
+
+    # faltou o ON para ligar os alunos e turma
+    
