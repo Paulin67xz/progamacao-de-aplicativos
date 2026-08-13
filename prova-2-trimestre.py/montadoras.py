d1da4adb2f16cb2f63e63e6ad51e991542ff7592
paulo <pauloarthurlimagomes2009@gmail.com>
2026-08-13 10:13:37 -0300

    13/08

---
+++ prova-2-trimestre.py/montadoras.py
@@ -0,0 +1,423 @@
+import sqlite3
+
+def cadastrar_montadoras():
+    try:
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        cursor.execute ('''
+                        CREATE TABLE IF NOT EXISTS montadoras (
+                    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    marca TEXT,
+                    pais_origem TEXT
+                    )
+                    ''')
+        
+
+        marca = input("Digite a marca da montadora: ")
+        pais_origem = input("Digite o país de origem: ")
+
+        comando_inserir = f'''
+        INSERT INTO montadoras (marca, pais_origem)
+        values ('{marca}', '{pais_origem}')
+        '''
+
+        cursor.execute(comando_inserir)
+
+        conexao.commit()
+
+        print("cadastro realizado")
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+def listar_montadoras():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM montadoras") 
+        listar_montadoras = cursor.fetchall()
+
+        print("=== Lista de Montadoras ===")
+
+        for montadora in listar_montadoras:
+            print(f"ID: {montadora[0]}")
+            print(f"Marca: {montadora[1]}")
+            print(f"país de origem: {montadora[2]}")
+            print("-" * 30)
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+def atualizar_montadoras():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        listar_montadoras()
+
+        id_montadora = int(input("Qual id deseja atualizar: "))
+
+        cursor.execute(f'''SELECT marca , pais_origem FROM montadoras WHERE id = {id_montadora} ''')
+
+        montadora = cursor.fetchone()
+
+        if not montadora:
+            print("Montadora não encontrada")
+            conexao.close()
+            return
+        else:
+            marca_atualizada = input(" Atualize a marca: ")
+            pais_origem_atualizada = input(" Atualize o país de origem: ")
+            
+            cursor.execute(f'''
+                           UPDATE montadoras 
+                           SET marca = '{marca_atualizada}', pais_origem = '{pais_origem_atualizada}'
+                           WHERE id = {id_montadora}
+                           ''')
+            
+            conexao.commit()
+            print(" Dados alterados ")
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+def deletar_montadora():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        listar_montadoras()
+
+        id_montadora = int(input("Qual id deseja deletar: "))
+
+        cursor.execute(f''' DELETE FROM montadoras WHERE id = {id_montadora}''')
+
+        conexao.commit()
+        print(" Montadora deletada")
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+
+
+def cadartrar_concessionarias():
+     try:
+
+            conexao = sqlite3.connect('veiculos.db')
+            cursor = conexao.cursor()
+
+            cursor.execute ('''
+                            CREATE TABLE IF NOT EXISTS concessionarias (
+                        id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                        cidade TEXT,
+                        id_montadora INTEGER
+                        )
+                        ''')
+            
+            cidade_concessionaria = input("Digite a cidade da sua concessionaria: ")
+            id_montadora = int(input("Digite o ID da montadora: "))
+
+
+
+            cursor.execute(f'''SELECT id FROM montadoras WHERE id = {id_montadora} ''')
+
+            montadora = cursor.fetchone()
+
+            if not montadora:
+                print("Montadora não encontrada")
+                conexao.close()
+                return
+            
+            comando_inserir = f'''
+            INSERT INTO concessionarias (cidade, id_montadora)
+            values ('{cidade_concessionaria}', '{id_montadora}')
+            '''
+
+            cursor.execute(comando_inserir)
+
+           
+            conexao.commit()
+            print("Cadastro realizado!")
+
+                
+
+     except ValueError:
+        print("Valor inválido.")
+     except TypeError:
+        print("Tipo de dado inválido.")
+     except IndexError:
+        print("Índice fora da lista.")
+     except KeyError:
+        print("Chave não encontrada.")
+     except FileNotFoundError:
+        print("Arquivo não encontrado.")
+     except PermissionError:
+        print("Permissão negada.")
+     except Exception as e:
+        print(f"Erro inesperado: {e}")
+     finally:
+        conexao.close()
+
+
+def listar_concessionaria():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM concessionarias") 
+        listar_concessionarias = cursor.fetchall()
+
+        print("=== Lista de Concessionarias ===")
+
+        for concessionaria in listar_concessionarias:
+            print(f"ID: {concessionaria[0]}")
+            print(f"Cidade: {concessionaria[1]}")
+            print(f"id montadora: {concessionaria[2]}")
+            print("-" * 30)
+
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+def atualizar_concessionaria():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        listar_concessionaria()
+
+        id_concessionaria = int(input("Qual id deseja atualizar: "))
+
+        cursor.execute(f'''SELECT cidade, id_montadora FROM concessionarias WHERE id = {id_concessionaria} ''')
+
+        concessionaria = cursor.fetchone()
+
+        if not concessionaria:
+            print("concessionaria não encontrada")
+            conexao.close()
+            return
+        else:
+            cidade_atualizada = input(" Atualize a cidade: ")
+            id_montadora_atualizado = int(input(" Atualize o id da montadora: "))
+
+            
+            cursor.execute(f'''
+                           UPDATE concessionarias
+                            SET cidade = '{cidade_atualizada}',
+                            id_montadora = {id_montadora_atualizado}
+                            WHERE id = {id_concessionaria}
+                            ''')
+            
+            
+            cursor.execute(f'''SELECT id FROM montadoras WHERE id = {id_montadora} ''')
+
+            montadora = cursor.fetchone()
+
+            if not montadora:
+                print("Montadora não encontrada")
+                conexao.close()
+                return
+
+
+
+
+            
+            conexao.commit()
+            print(" Dados alterados ")
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+def deletar_concessionaria():
+    try:
+
+        conexao = sqlite3.connect('veiculos.db')
+        cursor = conexao.cursor()
+
+        listar_concessionaria()
+
+        id_concessionaria = int(input("Qual id deseja deletar: "))
+
+        cursor.execute(f''' DELETE FROM concessionarias WHERE id = {id_concessionaria}''')
+
+        conexao.commit()
+        print(" Concessionaria deletada")
+
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    finally:
+        conexao.close()
+
+
+
+
+
+
+def menu_montadoras_e_concessionarias():
+    try:
+      
+        while True:
+            print("\n--- TABELA MONTADORAS ---")
+            print("1. Cadastrar Montadora") 
+            print("2. Listar Montadoras") 
+            print("3. Atualizar Montadora") 
+            print("4. Excluir Montadora") 
+
+            print("\n--- TABELA CONCESSIONARIAS ---")
+            print("5. Cadastrar Concessionaria") 
+            print("6. Listar Concessionarias") 
+            print("7. Atualizar Concessionaria") 
+            print("8. Excluir Concessionaria") 
+            
+            print("9. Sair")
+
+            opcao = input("Escolha uma opção: ")
+
+            if opcao == '1': cadastrar_montadoras()
+            elif opcao == '2': listar_montadoras() 
+            elif opcao == '3': atualizar_montadoras() 
+            elif opcao == '4': deletar_montadora() 
+
+            elif opcao == '5': cadartrar_concessionarias()
+            elif opcao == '6': listar_concessionaria()
+            elif opcao == '7': atualizar_concessionaria() 
+            elif opcao == '8': deletar_concessionaria() 
+            elif opcao == '9': break
+            else: print("Opção inválida!")
+
+    except ValueError:
+        print("Valor inválido.")
+    except TypeError:
+        print("Tipo de dado inválido.")
+    except IndexError:
+        print("Índice fora da lista.")
+    except KeyError:
+        print("Chave não encontrada.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except PermissionError:
+        print("Permissão negada.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
+
+menu_montadoras_e_concessionarias()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
